96f8c01129445287bb89605d609620de2930b254
bogatovnikita <GfiFhj57?,;)&gjJv57!>
2022-06-08 19:48:58 +0300

    add layout for recycler view

--- app/src/main/java/com/bogatovnikita/popularslibsandroidgb/MainActivity.kt
+++ app/src/main/java/com/bogatovnikita/popularslibsandroidgb/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.bogatovnikita.popularslibsandroidgb
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
